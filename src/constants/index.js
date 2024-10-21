91cdffa340f66d3732a4f60c65305c7bbde88bf2
Omkar Bharitkar <75433632+omkar2711@users.noreply.github.com>
2024-10-21 18:19:53 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -199,7 +199,7 @@ const projects = [
 
 const experiences = [
   {
-    title: "Software Developer Engineer",
+    title: "Software Developer Engineer Intern",
     company_name: "SyncSense",
     icon: syncsense,
     iconBg: "#131313",
@@ -224,32 +224,31 @@ const experiences = [
   //     "Participating in code reviews and providing constructive feedback to other developers.",
   //   ],
   // },
-  // {
-  //   title: "Web Developer",
-  //   company_name: "Shopify",
-  //   icon: shopify,
-  //   iconBg: "#383E56",
-  //   date: "Jan 2022 - Jan 2023",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
-  // {
-  //   title: "Full stack Developer",
-  //   company_name: "Meta",
-  //   icon: meta,
-  //   iconBg: "#E6DEDD",
-  //   date: "Jan 2023 - Present",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
+  {
+    title: "Technical Content Strategist Intern",
+    company_name: "upGrad",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "March 2024 - July 2024",
+    points: [
+      "Directed content development for a full stack development cohort, with a strong emphasis on practical skills.",
+      "Collaborated closely with industry experts to ensure the curriculum met current and evolving professional standards.",
+      "Utilized Java for implementing data structures and employed the MERN Stack for full-stack development projects.",
+    ],
+  },
+  {
+    title: "Associate - Content",
+    company_name: "upGrad",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "July 2024 - Present",
+    points: [
+      "Assisted senior instructors in teaching 80+ students across 4 batches in Full Stack Development.",
+      "Created curriculum, incorporating industry insights and practical case studies to enhance learning.",
+      "Led doubt-solving sessions and provided personal guidance to support learners in understanding key concepts.",
+      "Designed capstone web development projects to give students hands-on experience with real-world applications.",
+    ],
+  },
 ];
 
 export { services, technologies,experiences, Social, projects };
